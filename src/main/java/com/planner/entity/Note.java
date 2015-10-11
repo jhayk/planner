f157448aa57a8014c36fdc25cbf2bc92846042e9
Hayk <hayk.jhangiryan@gmail.com>
2015-10-11 22:38:40 +0200

    Created field has added to Note entity

--- src/main/java/com/planner/entity/Note.java
+++ src/main/java/com/planner/entity/Note.java
@@ -1,8 +1,12 @@
 package com.planner.entity;
 
+import java.util.Date;
+
 public class Note {
 
     private String text;
+
+    private Date created;
 
     public String getText() {
         return text;
@@ -10,5 +14,13 @@ public class Note {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
     }
 }
